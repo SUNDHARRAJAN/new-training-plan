14aaedfcc3ed23b252de0397d90ed5ae78541f8c
SUNDHARRAJAN <43337913+SUNDHARRAJAN@users.noreply.github.com>
2018-09-22 12:33:15 +0530

    Add files via upload

---
+++ JSStalkers(prompt,console).js
@@ -0,0 +1,7 @@
+var FirstName=prompt("what is your FirstName");
+var LastName=prompt("what is your LastName");
+var Age=prompt("what is your Age");
+
+var fullname= FirstName +" "+LastName;
+console.log("your full name is " + fullname);
+console.log("your Age is "+ Age);
---
+++ age_calculator.js
@@ -0,0 +1,5 @@
+var Age=prompt("what is your Age");
+
+var days= Age * 365;
+
+alert(Age + " Years is roughly "+ days +" days");
